8ad57c9a7f30570c1b11e72eea56c01b6fb788e8
Subhon Ghosh <subhonghosh@gmail.com>
2023-08-19 19:45:12 -0700

    making progress with some buttons

---
+++ src/components/Button.js
@@ -0,0 +1,95 @@
+import React from 'react';
+import './Button.css'; 
+import { Link } from 'react-router-dom';
+
+const STYLES = ['btn--primary', 'btn--outline'];
+
+const SIZES = ['btn--medium', 'btn--large'];
+
+export const Button = ({
+    children, 
+    type, 
+    onClick, 
+    buttonStyle, 
+    buttonSize
+}) => {
+    const checkButtonStyle = STYLES.includes(buttonStyle) ? buttonStyle : STYLES[0];
+    const checkButtonSize = STYLES.includes(buttonSize) ? buttonSize : SIZES[1];
+
+    return ( 
+        <Link to='/music' className='btn-mobile'>
+            <button
+            className={`btn ${checkButtonStyle} ${checkButtonSize}`}
+            onClick={onClick}
+            type={type}
+            >
+                {children}
+            </button>
+        </Link>
+)};
+
+export const Button2 = ({
+    children, 
+    type, 
+    onClick, 
+    buttonStyle, 
+    buttonSize
+}) => {
+    const checkButtonStyle = STYLES.includes(buttonStyle) ? buttonStyle : STYLES[0];
+    const checkButtonSize = STYLES.includes(buttonSize) ? buttonSize : SIZES[1];
+
+    return ( 
+        <Link to='/photography' className='btn-mobile'>
+            <button
+            className={`btn ${checkButtonStyle} ${checkButtonSize}`}
+            onClick={onClick}
+            type={type}
+            >
+                {children}
+            </button>
+        </Link>
+)};
+
+export const Button3 = ({
+    children, 
+    type, 
+    onClick, 
+    buttonStyle, 
+    buttonSize
+}) => {
+    const checkButtonStyle = STYLES.includes(buttonStyle) ? buttonStyle : STYLES[0];
+    const checkButtonSize = STYLES.includes(buttonSize) ? buttonSize : SIZES[1];
+
+    return ( 
+        <Link to='/programming' className='btn-mobile'>
+            <button
+            className={`btn ${checkButtonStyle} ${checkButtonSize}`}
+            onClick={onClick}
+            type={type}
+            >
+                {children}
+            </button>
+        </Link>
+)};
+
+export const Button4 = ({
+    children, 
+    type, 
+    onClick, 
+    buttonStyle, 
+    buttonSize
+}) => {
+    const checkButtonStyle = STYLES.includes(buttonStyle) ? buttonStyle : STYLES[0];
+    const checkButtonSize = STYLES.includes(buttonSize) ? buttonSize : SIZES[1];
+
+    return ( 
+        <Link to='/engineering' className='btn-mobile'>
+            <button
+            className={`btn ${checkButtonStyle} ${checkButtonSize}`}
+            onClick={onClick}
+            type={type}
+            >
+                {children}
+            </button>
+        </Link>
+)};
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,19 +1,61 @@
-import React, {useState} from 'react';
+import React, { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
+import { Button, Button2, Button3, Button4 } from './Button';
+import './Navbar.css';
 
 function Navbar() {
+  const [click, setClick] = useState(false);
+  const [button, setButton] = useState(true);
+
+  const handleClick = () => setClick(!click);
+  const closeMobileMenu = () => setClick(false);
+
+  const showButton = () => {
+    if (window.innerWidth <= 960) {
+      setButton(false);
+    } else {
+      setButton(true);
+    }
+  };
+
+  useEffect(() => {
+    showButton();
+    window.addEventListener('resize', showButton);
+    return () => {
+      window.removeEventListener('resize', showButton);
+    };
+  }, []);
+
   return (
-    // <div>Navbar</div>
-    <>
-        <nav className="navbar">
-            <div className="navbar-container">
-                <Link to="/" className="navbar-logo">
-                    Subhon Ghosh
-                </Link>
+    <nav className="navbar">
+      <div className="navbar-container">
+        <div className="logo-container">
+          <Link to="/" className="navbar-logo">
+            Subhon Ghosh
+          </Link>
+        </div>
+        <div className="menu-icon" onClick={handleClick}>
+          <i className={click ? 'fas fa-times' : 'fas fa-bars'} />
+        </div>
+        {button && (
+          <div className="nav-buttons">
+            <div className="nav-button-container">
+              <Button buttonStyle="btn--outline">Music</Button>
             </div>
-        </nav>
-    </>
-  )
+            <div className="nav-button-container">
+              <Button2 buttonStyle="btn--outline">Photography</Button2>
+            </div>
+            <div className="nav-button-container">
+              <Button3 buttonStyle="btn--outline">Programming</Button3>
+            </div>
+            <div className="nav-button-container">
+              <Button4 buttonStyle="btn--outline">Engineering</Button4>
+            </div>
+          </div>
+        )}
+      </div>
+    </nav>
+  );
 }
 
-export default Navbar
+export default Navbar;
